49273687c910ab7eeef8a5cbdce1f7a9b7fb7c7f
UmmuRufauda <mariamabdallah711@gmail.com>
2022-03-09 18:44:22 +0300

    added delete.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <link rel="stylesheet" href="css/hrstyle.css">
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
-
     <script src="bootstrap/js/bootstrap.min.js"></script>
     <script src="js/popper.min.js"></script>
     <script src="js/jquery.min.js"></script>
--- singleOrder.php
+++ singleOrder.php
@@ -27,34 +27,56 @@ if (isset($_GET["id"]) and !empty($_GET["id"])) {
             $filepath = "uploads/$photo";
 
             ?>
-<div class="row m-2">
-    <div class="card col-md-3 m-2">
-        <div class="card-body">
-            <div class="m-3 p-3">
-                <img src="<?php echo $filepath?>" alt="Loading" height="150" width="150" class="rounded-circle">
 
-            </div>
-        </div>
-        <div class="card col-md-6 m-2 bg-primary text-white">
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>single Order</title>
+    <link rel="stylesheet" href="css/hrstyle.css">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/jquery.min.js"></script>
+</head>
+
+<body>
+
+
+    <div class="row m-2">
+        <div class="card col-md-3 m-2">
             <div class="card-body">
-                <div>
-                    <label class="form-label h6">FULL NAME</label>
-                    <p><?php echo $fullname; ?></p>
+                <div class="m-3 p-3">
+                    <img src="<?php echo $filepath?>" alt="Loading" height="150" width="150" class="rounded-circle">
+
                 </div>
-                <hr>
-                <div>
-                    <label class="form-label h6">EMAIL</label>
-                    <p><?php echo $email; ?></p>
-                </div>
-                <hr>
-                <div>
-                    <label class="form-label h6">Detail</label>
-                    <p><?php echo  $message; ?></p>
+            </div>
+            <div class="card col-md-6 m-2 bg-primary text-white">
+                <div class="card-body">
+                    <div>
+                        <label class="form-label h6">FULL NAME</label>
+                        <p><?php echo $fullname; ?></p>
+                    </div>
+                    <hr>
+                    <div>
+                        <label class="form-label h6">EMAIL</label>
+                        <p><?php echo $email; ?></p>
+                    </div>
+                    <hr>
+                    <div>
+                        <label class="form-label h6">Detail</label>
+                        <p><?php echo  $message; ?></p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <?php
+        <?php
 
         } else {
             echo "No record was found";
@@ -68,3 +90,6 @@ if (isset($_GET["id"]) and !empty($_GET["id"])) {
     echo "id value not picked";
 }
 ?>
+</body>
+
+</html>
